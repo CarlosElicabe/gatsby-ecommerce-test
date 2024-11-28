2a37a8fd0c2e114114e203b1b447dbb1e41bc4a2
CarlosElicabe <118000516+CarlosElicabe@users.noreply.github.com>
2024-11-28 15:50:45 -0300

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,4 @@ import { NotificationProvider } from './src/context/AddItemNotificationProvider'
 export const wrapRootElement = ({ element }) => (
   <NotificationProvider>{element}</NotificationProvider>
 );
-/* don't use TEST1 test1 */
+/*  don't use TEST1 test1 */
